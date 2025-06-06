afffbc008750d75f552697ba37d3c0cfee58b99b
sandond77 <sandond77@gmail.com>
2025-06-06 11:15:42 -0700

    made a basic mongoose model for campgrounds and tested post route

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const ejs = require('ejs');
+const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', ejs);
+const Campground = require('./models/campground');
+
+main().catch((err) => console.log(`connection error: ${err}`));
+
+async function main() {
+	await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+	console.log('connected to mongod');
+	// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	res.render('home');
+});
+
+app.get('/makeCampground', async (req, res) => {
+	const camp = new Campground({
+		title: 'test1',
+		description: 'this is a sample description'
+	});
+
+	await camp
+		.save()
+		.then(res.send(camp))
+		.catch((err) => `error occured: ${err}`);
 });
 
 app.listen(port, () => {
