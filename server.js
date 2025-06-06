6ace719d2cfb79f210385d2a54af8d87b4d02c5e
sandond77 <sandond77@gmail.com>
2025-06-06 12:36:43 -0700

    created route for adding a new campground with some basic navigation buttons for pages

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const Campground = require('./models/campground');
-
+//section for mongoose connection and database connection
 main().catch((err) => console.log(`connection error: ${err}`));
 
 async function main() {
@@ -14,11 +13,13 @@ async function main() {
 	console.log('connected to mongod');
 	// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+const Campground = require('./models/campground');
 
+//EJS setup with express and folder directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/views'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true })); //allows express to parse json
+app.use(express.static(__dirname + '/public')); //for serving static pages;
 
 app.get('/', (req, res) => {
 	res.render('home');
@@ -29,9 +30,19 @@ app.get('/campgrounds', async (req, res) => {
 	res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req, res) => {
+	res.render('campgrounds/new');
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	res.render('campgrounds/show', { campground });
+});
+
+app.post('/campgrounds', async (req, res) => {
+	const campground = new Campground(req.body.campground);
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(port, () => {
