03ed6837bf7e4947b016a46c989efbab71deedb8
sandond77 <sandond77@gmail.com>
2025-06-06 12:59:27 -0700

    added edit and update route for individual campgrounds

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOveride = require('method-override');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/views'));
 app.use(express.urlencoded({ extended: true })); //allows express to parse json
 app.use(express.static(__dirname + '/public')); //for serving static pages;
+app.use(methodOveride('_method')); //for using different crud methods on form submission
 
 app.get('/', (req, res) => {
 	res.render('home');
@@ -28,6 +30,12 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
 	const campgrounds = await Campground.find({});
 	res.render('campgrounds/index', { campgrounds });
+});
+
+app.post('/campgrounds', async (req, res) => {
+	const campground = new Campground(req.body.campground);
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds/new', (req, res) => {
@@ -39,10 +47,17 @@ app.get('/campgrounds/:id', async (req, res) => {
 	res.render('campgrounds/show', { campground });
 });
 
-app.post('/campgrounds', async (req, res) => {
-	const campground = new Campground(req.body.campground);
-	await campground.save();
-	res.redirect(`/campgrounds/${campground._id}`);
+app.put('/campgrounds/:id', async (req, res) => {
+	const id = req.params.id;
+	const campground = await Campground.findByIdAndUpdate(id, {
+		...req.body.campground
+	});
+	res.redirect(`/campgrounds/${id}`);
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render('campgrounds/edit', { campground });
 });
 
 app.listen(port, () => {
