c3c807ffee014855c0ef72294913843fda2096bd
sandond77 <sandond77@gmail.com>
2025-06-16 18:46:10 -0700

    refactored joi campground schema to a standalone file

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Joi = require('joi');
+const { campgroundValidationSchema } = require('./validationSchemas');
 
 //section for mongoose connection and database connection
 main().catch((err) => console.log(`connection error: ${err}`));
@@ -29,18 +30,7 @@ app.use(express.static(__dirname + '/public')); //for serving static pages;
 app.use(methodOveride('_method')); //for using different crud methods on form submission
 
 const validateCampground = (req, res, next) => {
-	const campgroundSchema = Joi.object({
-		campground: Joi.object({
-			title: Joi.string().required(),
-			price: Joi.number().required().min(0),
-			image: Joi.string().required(),
-			location: Joi.string().required(),
-			description: Joi.string().required()
-		}).required()
-	});
-
-	const { error } = campgroundSchema.validate(req.body);
-	console.log(error);
+	const { error } = campgroundValidationSchema.validate(req.body);
 	if (error) {
 		const msg = error.details.map((el) => el.message).join(',');
 		throw new ExpressError(msg, 400);
