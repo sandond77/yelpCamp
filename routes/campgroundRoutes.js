ab5afa7e0c4753862a85ca30660a3c929af0cb27
sandond77 <sandond77@gmail.com>
2025-06-30 10:57:58 -0700

    added author to campground pages and saved author to newly created campgrounds

--- routes/campgroundRoutes.js
+++ routes/campgroundRoutes.js
@@ -32,6 +32,7 @@ router.post(
 	isLoggedIn,
 	catchAsync(async (req, res, next) => {
 		const campground = new Campground(req.body.campground);
+		campground.author = req.user._id;
 		await campground.save();
 		req.flash('success', 'Successfully made a new campground!');
 		res.redirect(`/campgrounds/${campground._id}`);
@@ -45,9 +46,9 @@ router.get('/new', isLoggedIn, (req, res) => {
 router.get(
 	'/:id',
 	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id).populate(
-			'reviews'
-		);
+		const campground = await Campground.findById(req.params.id)
+			.populate('reviews')
+			.populate('author');
 		if (!campground) {
 			req.flash('error', 'Campground does not exist!');
 			return res.redirect('/campgrounds');
