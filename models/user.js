681e8c034045ccec2fcd55d91c5b28f7af3062a4
sandond77 <sandond77@gmail.com>
2025-06-29 23:02:24 -0700

    added error handling to registration route

--- models/user.js
+++ models/user.js
@@ -3,6 +3,11 @@ const { Schema } = mongoose;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
 	email: {
 		type: String,
 		required: true,
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register', (req, res) => {
 	res.render('users/register');
 });
 
-router.post('/register', (req, res) => {
-	res.send(req.body);
-});
+router.post(
+	'/register',
+	catchAsync(async (req, res) => {
+		try {
+			const { username, email, password } = req.body;
+			const newUser = new User({ username, email });
+			const registeredUser = await User.register(newUser, password);
+			console.log(registeredUser);
+			req.flash('success', 'You have successfully registered for Yelpcamp!');
+			res.redirect('/campgrounds');
+		} catch (error) {
+			req.flash('error', error.message);
+			res.redirect('register');
+		}
+	})
+);
 
 module.exports = router;
