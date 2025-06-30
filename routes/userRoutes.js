d3f50358bdd615ecb75ea84503efee91806c0f54
sandond77 <sandond77@gmail.com>
2025-06-29 23:46:48 -0700

    added logout route and login buttons to navbar

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -40,4 +40,14 @@ router.post(
 	}
 );
 
+router.get('/logout', (req, res, next) => {
+	req.logout((err) => {
+		if (err) {
+			return next(err);
+		}
+		req.flash('success', 'You have successfully logged out!');
+		res.redirect('/campgrounds');
+	});
+});
+
 module.exports = router;
