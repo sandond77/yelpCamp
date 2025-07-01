fa72fc0b55554745b126901c6066884dde17afbf
sandond77 <sandond77@gmail.com>
2025-07-01 12:01:08 -0700

    refactored campgroundroutes to use .route

--- routes/campgroundRoutes.js
+++ routes/campgroundRoutes.js
@@ -4,39 +4,33 @@ const catchAsync = require('../utils/catchAsync');
 const campgrounds = require('../controllers/campgroundController.js');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 
-router.get('/', catchAsync(campgrounds.index));
-
-router.post(
-	'/',
-	validateCampground,
-	isLoggedIn,
-	catchAsync(campgrounds.createCampground)
-);
+// prettier-ignore
+router.route('/')
+	.get(catchAsync(campgrounds.index))
+	.post(
+		validateCampground,
+		isLoggedIn,
+		catchAsync(campgrounds.createCampground)
+	);
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router
+	.route('/:id')
+	.get(catchAsync(campgrounds.showCampground))
+	.put(
+		validateCampground,
+		isLoggedIn,
+		isAuthor,
+		catchAsync(campgrounds.editCampground)
+	)
+	.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get(
 	'/:id/edit',
 	isLoggedIn,
 	isAuthor,
 	catchAsync(campgrounds.renderEditForm)
-);
-
-router.put(
-	'/:id',
-	validateCampground,
-	isLoggedIn,
-	isAuthor,
-	catchAsync(campgrounds.editCampground)
-);
-
-router.delete(
-	'/:id',
-	isLoggedIn,
-	isAuthor,
-	catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
