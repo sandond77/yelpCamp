e4f42903fe61d21f0771c12e6a06e81dcb32f14f
sandond77 <sandond77@gmail.com>
2025-07-03 11:04:16 -0700

    added pin and center map to campground location on map per campsite

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -3,6 +3,11 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 	container: 'map', // container ID
 	style: 'mapbox://styles/mapbox/streets-v12', // style URL
-	center: [-74.5, 40], // starting position [lng, lat]
+	center: campground.geometry.coordinates, // starting position [lng, lat]
 	zoom: 9 // starting zoom
 });
+
+// Set marker options.
+const marker = new mapboxgl.Marker()
+	.setLngLat(campground.geometry.coordinates)
+	.addTo(map);
