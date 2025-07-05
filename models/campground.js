2967f27542bf3b09895ab59790524833668c83e6
sandond77 <sandond77@gmail.com>
2025-07-05 00:53:29 -0700

    added custom popups on cluster map for each campground;

--- models/campground.js
+++ models/campground.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const { Schema } = mongoose;
 
+const opts = { toJSON: { virtuals: true } };
+
 const ImageSchema = new Schema({
 	url: String,
 	filename: String
@@ -11,33 +13,43 @@ ImageSchema.virtual('thumbnail').get(function () {
 	return this.url.replace('/upload', '/upload/w_200');
 });
 
-const CampgroundSchema = new Schema({
-	title: String,
-	images: [ImageSchema],
-	price: Number,
-	description: String,
-	location: String,
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User'
-	},
-	geometry: {
-		type: {
-			type: String,
-			enum: ['Point'],
-			required: true
-		},
-		coordinates: {
-			type: [Number],
-			required: true
-		}
-	},
-	reviews: [
-		{
+const CampgroundSchema = new Schema(
+	{
+		title: String,
+		images: [ImageSchema],
+		price: Number,
+		description: String,
+		location: String,
+		author: {
 			type: Schema.Types.ObjectId,
-			ref: 'Review'
-		}
-	]
+			ref: 'User'
+		},
+		geometry: {
+			type: {
+				type: String,
+				enum: ['Point'],
+				required: true
+			},
+			coordinates: {
+				type: [Number],
+				required: true
+			}
+		},
+		reviews: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Review'
+			}
+		]
+	},
+	opts
+);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+	return `
+		<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+		<p>${this.description.substring(0, 30)}</p>
+		`;
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
--- public/scripts/clusterMap.js
+++ public/scripts/clusterMap.js
@@ -100,11 +100,7 @@ map.on('load', () => {
 	// description HTML from its properties.
 	map.on('click', 'unclustered-point', (e) => {
 		const coordinates = e.features[0].geometry.coordinates.slice();
-		const mag = e.features[0].properties.mag;
-		const tsunami =
-			e.features[0].properties.tsunami === 1
-				? 'sl;dfjdasl;fasfd'
-				: 'kasdhfksdfa';
+		const { popUpMarkup } = e.features[0].properties;
 
 		// Ensure that if the map is zoomed out such that
 		// multiple copies of the feature are visible, the
@@ -115,10 +111,7 @@ map.on('load', () => {
 			}
 		}
 
-		new mapboxgl.Popup()
-			.setLngLat(coordinates)
-			.setHTML(`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`)
-			.addTo(map);
+		new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
 	});
 
 	map.on('mouseenter', 'clusters', () => {
